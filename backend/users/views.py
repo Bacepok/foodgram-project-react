8d1df2a60e9ac815d457c745e05dea8ce449c00f
Дмитрий <vaser1@yandex.ru>
2023-04-28 05:22:23 +0300

    Rev 3.00

--- backend/users/views.py
+++ backend/users/views.py
@@ -5,7 +5,8 @@ from djoser.views import UserViewSet
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.permissions import (IsAuthenticatedOrReadOnly,
+                                        IsAuthenticated,)
 from rest_framework.response import Response
 
 from .models import Subscribe
