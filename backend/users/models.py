5a6e79e25ed446e855f2ccb839d1b09ba95f2401
Дмитрий <vaser1@yandex.ru>
2023-04-28 06:05:29 +0300

    Rev 3.99

--- backend/users/models.py
+++ backend/users/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
 
     class Meta:
         ordering = ['id']
-        verbose_name = 'Пользователь'
+        verbose_name = 'Пользователь.'
         verbose_name_plural = 'Пользователи'
 
     def __str__(self):
