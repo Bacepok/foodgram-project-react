438196328bb5aee8f3a7327bc0884a4113d187e1
Дмитрий <vaser1@yandex.ru>
2023-04-28 04:30:02 +0300

    Rev 3.00

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,59 +1,95 @@
-from django.contrib.admin import ModelAdmin, TabularInline, site
-from django.contrib.auth.admin import UserAdmin
+from django.contrib import admin
 
-from .models import (Ingredient, IngredientsInRecipe, Recipe, Tag,
-                     TagsInRecipe, User)
-
-
-class RecipeIngredientInline(TabularInline):
-    model = IngredientsInRecipe
-    min_num = 1
+from .models import (Favorite, Ingredient, IngredientInRecipe,
+                     Recipe, Tag, ShoppingCart)
 
 
-class IngredientAdmin(ModelAdmin):
-    list_display = ('id', 'name', 'measurement_unit')
-    list_filter = ('name', 'measurement_unit')
-    list_editable = ('name', 'measurement_unit')
-
-
-class RecipeAdmin(ModelAdmin):
-    list_display = ('id', 'name', 'author', 'cooking_time',)
-    search_fields = ('text', 'name')
-    list_editable = ('author', 'name', 'cooking_time')
+class BaseAdminSettings(admin.ModelAdmin):
+    """Базовая кастомизация админ панели."""
+    empty_value_display = '-пусто-'
     list_filter = ('author', 'name', 'tags')
-    inlines = (RecipeIngredientInline,)
 
 
-class TagAdmin(ModelAdmin):
-    list_display = ('id', 'name', 'slug', 'color')
-    list_editable = ('name', 'slug', 'color')
-    search_fields = ('name', 'slug', 'color')
+class TagAdmin(BaseAdminSettings):
+    """
+    Кастомизация админ панели (управление тегами).
+    """
+    list_display = (
+        'name',
+        'color',
+        'slug'
+    )
+    list_display_links = ('name',)
+    search_fields = ('name',)
+    list_filter = ('name',)
 
 
-class TagsInRecipeAdmin(ModelAdmin):
-    list_display = ('id', 'recipe', 'tag')
-    list_editable = ('recipe', 'tag')
+class IngredientAdmin(BaseAdminSettings):
+    """
+    Кастомизация админ панели (управление ингредиентами).
+    """
+    list_display = (
+        'name',
+        'measurement_unit'
+    )
+    # list_display_links = ('name',)
+    # search_fields = ('name',)
+    list_filter = ('name',)
 
 
-class IngredientsInRecipeAdmin(ModelAdmin):
-    list_display = ('id', 'recipe', 'amount', 'ingredient')
-    list_editable = ('recipe', 'amount', 'ingredient')
+class RecipeAdmin(BaseAdminSettings):
+    """
+    Кастомизация админ панели (управление рецептами).
+    """
+    list_display = (
+        'name',
+        'author',
+        'added_in_favorites'
+    )
+    list_display_links = ('name',)
+    search_fields = ('name',)
+    list_filter = ('author', 'name', 'tags')
+    readonly_fields = ('added_in_favorites',)
+    filter_horizontal = ('tags',)
+
+    def added_in_favorites(self, obj):
+        return obj.favorites.all().count()
+
+    added_in_favorites.short_description = 'Количество добавлений в избранное'
 
 
-class CustomUserAdmin(UserAdmin):
-    list_filter = UserAdmin.list_filter + ('email', 'username')
+class IngredientInRecipeAdmin(admin.ModelAdmin):
+    """
+    Кастомизация админ панели (управление ингридиентами в рецептах).
+    """
+    list_display = (
+        'ingredient',
+        'amount',
+    )
+    list_filter = ('ingredient',)
 
 
-PAIRS = [
-    (Tag, TagAdmin),
-    (Ingredient, IngredientAdmin),
-    (Recipe, RecipeAdmin),
-    (IngredientsInRecipe, IngredientsInRecipeAdmin),
-    (TagsInRecipe, TagsInRecipeAdmin),
-    (User, CustomUserAdmin),
-]
+class FavoriteAdmin(admin.ModelAdmin):
+    """
+    Кастомизация админ панели (управление избранными рецептами).
+    """
+    list_display = ('user', 'recipe')
+    list_filter = ('user', 'recipe')
+    search_fields = ('user', 'recipe')
 
-site.unregister(User)
 
-for i in PAIRS:
-    site.register(*i)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    """
+    Кастомизация админ панели (управление избранными рецептами).
+    """
+    list_display = ('recipe', 'user')
+    list_filter = ('recipe', 'user')
+    search_fields = ('user',)
+
+
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(IngredientInRecipe, IngredientInRecipeAdmin)
+admin.site.register(Favorite, FavoriteAdmin)
+admin.site.register(ShoppingCart, ShoppingCartAdmin)
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,27 +1,38 @@
 from django.contrib import admin
 
-from .models import Follow, User
+from .models import Subscribe, User
 
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class BaseAdminSettings(admin.ModelAdmin):
+    """Базовая кастомизация админ панели."""
+    empty_value_display = '-пусто-'
+    list_filter = ('email', 'username')
+
+
+class UsersAdmin(BaseAdminSettings):
+    """Кастомизация админ панели (управление пользователями)."""
     list_display = (
         'id',
+        'role',
         'username',
         'email',
         'first_name',
-        'last_name')
-    search_fields = ('username', 'email')
-    list_filter = ('username', 'email')
-    empty_value_display = '-пусто-'
+        'last_name'
+    )
+    list_display_links = ('id', 'username')
+    search_fields = ('role', 'username')
 
 
-@admin.register(Follow)
-class FollowAdmin(admin.ModelAdmin):
+class SubscribeAdmin(admin.ModelAdmin):
+    """Кастомизация админ панели (управление подписками)."""
     list_display = (
         'id',
-        'following',
-        'user')
-    search_fields = ('following', 'user')
-    list_filter = ('following', 'user')
-    empty_value_display = '-пусто-'
+        'user',
+        'author'
+    )
+    list_display_links = ('id', 'user')
+    search_fields = ('user',)
+
+
+admin.site.register(User, UsersAdmin)
+admin.site.register(Subscribe, SubscribeAdmin)
